ece48857e813de3d1efaa01bf12f7ad7408571cf
Tyler Copeland <copelandmedia@gmail.com>
2017-07-17 13:41:22 -0400

    Merge pull request #10 from riadvice/master
    Add new icons

--- export/bbb-icons.js
+++ export/bbb-icons.js
@@ -65,7 +65,11 @@ var glyphs = {
 	'romms': 'e93f',
 	'unlock': 'e940',
 	'record': 'e941',
-	'network': 'e942'
+	'network': 'e942',
+	'redo': 'e943',
+	'thumbs_down_filled': 'e944',
+	'thumbs_up_filled': 'e945',
+	'checkmark': 'e946'
 };
 
 $(document).ready(
